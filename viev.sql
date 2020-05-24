680a23db31d2c05a1f4d15cb35047a55cc3d97c0
Igor Bitsan <43330739+Igor1488228@users.noreply.github.com>
2020-05-25 00:23:45 +0300

    Update viev.sql

--- viev.sql
+++ viev.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE VIEW University_stat as
-    select university.university_name
-    , years.position
-    , years.year
-    , country.country
-    from university
-    join years on years.university_name = university.university_name
-    join country on country.university_name = university.university_name;
+CREATE VIEW Countries_Universities AS
+    SELECT
+        u.university_name,
+        u.dynamic_year,
+        c.country_name
+    FROM
+        countries c
+        LEFT JOIN universities u ON c.country_name = u.country_name;

d1338d88b1dc64c2041fb599091606844e8edd84
Igor Bitsan <43330739+Igor1488228@users.noreply.github.com>
2020-05-25 02:25:07 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,22 +1,75 @@
-CREATE TABLE University(
-    university_name VARCHAR(256) NOT NULL
-    , country VARCHAR(256) NOT NULL
-    , position VARCHAR(256) NOT NULL
-    , year NUMBER(4,0) NOT NULL
-    , CONSTRAINT university_pk PRIMARY KEY (university_name)
+CREATE TABLE universities (
+    university_name     VARCHAR2(50) NOT NULL,
+    dynamic_year   NUMBER(4) NOT NULL,
+    national_rank  NUMBER(3) NOT NULL,
+    country_name  VARCHAR2(50) NOT NULL
 );
-CREATE TABLE Years
-(
-    university_name VARCHAR(256) NOT NULL
-    ,year NUMBER(4,0) NOT NULL
-    , position VARCHAR(100) NOT NULL
-    , CONSTRAINT year_university_fk PRIMARY KEY (university_name)
-    ,CONSTRAINT year_pk FOREIGN KEY (university_name) REFERENCES university(university_name)
+
+ALTER TABLE universities
+    ADD CONSTRAINT universities_pk PRIMARY KEY ( university_name,
+                                          dynamic_year,
+                                          country_name );
+
+CREATE TABLE universities_ranks (
+    university_rank    NUMBER(3,0) NOT NULL,
+    university_name     VARCHAR2(50) NOT NULL,
+    dynamic_year   NUMBER(4) NOT NULL,
+    country_name  VARCHAR2(50) NOT NULL
 );
-CREATE TABLE Country
-(
-    university_name VARCHAR(256) NOT NULL,
-    country VARCHAR(256) NOT NULL,
-    CONSTRAINT country_pk PRIMARY KEY (country),
-    CONSTRAINT country_pk FOREIGN KEY (university_name) REFERENCES Years(university_name)
+
+ALTER TABLE universities_ranks
+    ADD CONSTRAINT universities_ranks_pk PRIMARY KEY ( university_rank,
+                                                 university_name,
+                                                 dynamic_year,
+                                                 country_name );
+
+CREATE TABLE countries (
+    country_name VARCHAR2(50) NOT NULL
 );
+
+ALTER TABLE countries ADD CONSTRAINT countries_pk PRIMARY KEY ( country_name );
+
+CREATE TABLE quality_of_faculty (
+    publications_number NUMBER NOT NULL,
+    quality_of_faculty          INTEGER,
+    university_name     VARCHAR2(50) NOT NULL,
+    dynamic_year   NUMBER(4) NOT NULL,
+    country_name  VARCHAR2(50) NOT NULL
+);
+
+ALTER TABLE quality_of_faculty
+    ADD CONSTRAINT quality_of_faculty_pk PRIMARY KEY (publications_number,
+                                         university_name,
+                                         dynamic_year,
+                                         country_name );
+
+CREATE TABLE ranks (
+    university_rank NUMBER(3,0) NOT NULL
+);
+
+ALTER TABLE ranks ADD CONSTRAINT ranks_pk PRIMARY KEY ( university_rank );
+
+ALTER TABLE universities
+    ADD CONSTRAINT universities_countries_fk FOREIGN KEY ( country_name )
+        REFERENCES countries ( country_name );
+
+ALTER TABLE universities_ranks
+    ADD CONSTRAINT universities_ranks_fk1 FOREIGN KEY ( university_name,
+                                                  dynamic_year,
+                                                  country_name )
+        REFERENCES universities ( university_name,
+                                  dynamic_year,
+                                  country_name );
+
+ALTER TABLE universities_ranks
+    ADD CONSTRAINT universities_ranks_fk2 FOREIGN KEY ( university_rank )
+        REFERENCES ranks ( university_rank );
+
+ALTER TABLE quality_of_faculty
+    ADD CONSTRAINT quality_of_faculty_universities_fk FOREIGN KEY ( university_name,
+                                               dynamic_year,
+                                               country_name )
+        REFERENCES universities ( university_name,
+                            dynamic_year,
+                           country_name );
+
